9db8350251edf41ed7570f0050c4d5fdc8084206
intarsV <initex@inbox.lv>
2019-05-22 10:32:29 +0300

    added TaskControllerTest

--- src/test/java/com/accenture/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/accenture/tasks/controller/TaskControllerTest.java
@@ -3,7 +3,6 @@ package com.accenture.tasks.controller;
 import com.accenture.tasks.domain.TaskStatusEnum;
 import com.accenture.tasks.dto.TaskDTO;
 import com.accenture.tasks.services.TaskService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,11 +17,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = TaskController.class)
@@ -47,7 +44,7 @@ public class TaskControllerTest {
         List<TaskDTO> response = new ArrayList();
         response.add(taskDTO);
         when(this.taskService.getAllTasks()).thenReturn(response);
-        mockMvc.perform(get("/tasks")).andExpect(status().isOk())
+        mockMvc.perform(get("/api/v1/tasks")).andExpect(status().isOk())
                 .andExpect(content().json("[{\"id\":1,\"taskTitle\":\"new special task\",\"statusEnum\":\"ACTIVE\"}]"));
     }
 
@@ -55,7 +52,7 @@ public class TaskControllerTest {
     public void shouldAddTaskDTO() throws Exception {
         when(this.taskService.saveTask(any(TaskDTO.class))).thenReturn(100L);
         String body = "{\"taskTitle\":\"megatask\"}";
-        mockMvc.perform(post("/tasks")
+        mockMvc.perform(post("/api/v1/tasks")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(body))
                 .andExpect(status().isCreated())
@@ -66,7 +63,7 @@ public class TaskControllerTest {
     public void shouldDeleteTaskDTO() throws Exception {
         final long taskId = 7L;
 
-        mockMvc.perform(delete("/tasks/" + taskId))
+        mockMvc.perform(delete("/api/v1/tasks/" + taskId))
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"id\":" + taskId + ",\"taskTitle\":null,\"statusEnum\":null}"));
     }
