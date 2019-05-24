e6e0512cd142710d17f44d164db4bd420a4b2597
intarsV <intars.vaicuks@gmail.com>
2019-05-24 14:24:00 +0300

    removed findAll method

--- src/main/java/com/accenture/tasks/repository/TaskRepository.java
+++ src/main/java/com/accenture/tasks/repository/TaskRepository.java
@@ -3,8 +3,6 @@ package com.accenture.tasks.repository;
 import com.accenture.tasks.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TaskRepository extends JpaRepository<Task, Long> {
-        List<Task> findAll();
+
 }
